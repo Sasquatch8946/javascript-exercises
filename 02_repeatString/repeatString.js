439393fc72d34e841c9131591232d72280669f2b
Sean M. Chapman <schapman@hennessyroach.com>
2024-02-18 18:36:38 -0600

    solve 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+    let result = '';
 
+    if (number > 0) {
+        result = string;
+    } else if (number === 0) {
+        return result;
+    } else {
+        return "ERROR";
+    }
+
+    for (let i = 1; i < number; i++) {
+        result += string;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
