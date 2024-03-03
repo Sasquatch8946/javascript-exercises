562df8767469c73b94711455ffaaf308a96a0d79
Sean M. Chapman <schapman@hennessyroach.com>
2024-03-03 08:35:30 -0600

    solve exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...items) {
+    let newArr = Array.from(arguments[0]);
+    let indx;
+    items.forEach((i) => {
+        indx = newArr.indexOf(i);
+        while (indx > -1) {
+            newArr.splice(indx, 1);
+            indx = newArr.indexOf(i);
+        }
+    });
 
+    return newArr;
 };
 
 // Do not edit below this line
