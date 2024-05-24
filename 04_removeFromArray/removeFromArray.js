9758c9c9cef15c1b0a9f99fab0e18119aa9a1c5e
Sasquatch8946 <42752821+Sasquatch8946@users.noreply.github.com>
2024-05-23 22:19:02 -0500

    solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...items) {
+    items.forEach((item) =>{
+        while (array.indexOf(item) > -1) {
+            indx = array.indexOf(item);
+            array.splice(indx, 1);
+        }
+    });
 
+    return array;
 };
 
 // Do not edit below this line
