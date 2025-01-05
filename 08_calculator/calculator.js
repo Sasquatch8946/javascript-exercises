3fd4245489ef37f789b743fefcb6d051b586615a
Sasquatch8946 <42752821+Sasquatch8946@users.noreply.github.com>
2025-01-05 09:21:42 -0600

    complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -10,16 +10,20 @@ const sum = function(nums) {
 	return nums.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((total, num) => total * num);
 };
 
-const power = function() {
-	
+const power = function(num, exponent) {
+	return num ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+  for (let i=1; i<num+1; i++) {
+    total = total * i;
+  }
+  return total;
 };
 
 // Do not edit below this line
