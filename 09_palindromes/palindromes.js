289bfb4cee93b258edf1cea97416fb4da9e6d954
Sasquatch8946 <42752821+Sasquatch8946@users.noreply.github.com>
2025-01-05 09:37:34 -0600

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (aStr) {
+    //create array of characters
+    const lettersOnly = aStr.replace(/\s+/g, "").replace(/[!,.\?]/g, "").toLowerCase();
+    const reversedStr = lettersOnly.split("").reverse().join("");
+    return lettersOnly === reversedStr
 };
 
 // Do not edit below this line
